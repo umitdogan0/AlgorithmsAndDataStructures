c0b31513666778d108d24f53aca8b65ee59c6be6
ümit doğan <umit9551@gmail.com>
2023-11-26 00:47:43 +0300

    first commit

---
+++ Algorithms/Searching.BinarySearch/Program.cs
@@ -0,0 +1,29 @@
+﻿bool BinarySearch(int[] array , int value)
+{
+    int left = 0;
+    int right = array.Length - 1;
+    
+
+    while (left <= right)
+    {
+        
+        int mid = left + (right - left) / 2;
+        if (array[mid] == value)
+        {
+            return true;
+        }
+        if (array[mid] < value )
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid + 1;
+        }
+    }
+
+    return false;
+}
+
+
+Console.WriteLine(BinarySearch(new []{1,2,3,4,5} , 7));
---
+++ Algorithms/Sorting.BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿T[] BubbleSort<T>(T[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - i - 1; j++)
+        {
+            if (Comparer<T>.Default.Compare(array[j],array[j + 1]) > 0)
+            {
+                (array[j], array[j + 1]) = (array[j + 1], array[j]);
+            }
+        }
+    }
+
+    return array;
+}
+
+foreach (var item in BubbleSort<int>(new[]{15,8,55,0,7,-1}))
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("-----------------------------------------");
+
+foreach (var item in BubbleSort<string>(new[]{"Ahmet" , "Zeynep" , "Emre" , "Barış" , "ümit"}))
+{
+    Console.WriteLine(item);
+}
